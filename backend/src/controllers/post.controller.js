33b9fbb5f17f60a66aab28c85e174f0b5bc3dd01
Raunak Dubey <raunak18@proton.me>
2026-02-13 17:32:17 +0530

    Post Creation done

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -2,15 +2,28 @@ import postModel from "../models/post.model.js";
 import ImageKit, { toFile } from "@imagekit/nodejs";
 
 const client = new ImageKit({
-    privateKey: process.env.IMAGEKIT_PRIVATE_KEY
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY
 })
 
 export const createPostController = async (req, res) => {
-  console.log(req.body, req.file);
-  const file = await client.files.upload({
-    file: await toFile(Buffer.from(req.file.buffer), 'file'),
-    fileName: 'test',
-  });
+  try {
+    const file = await client.files.upload({
+      file: await toFile(Buffer.from(req.file.buffer), 'file'),
+      fileName: req.file.originalname,
+      folder: 'instagram-posts'
+    });
 
-  res.send(file)
+    const post = await postModel.create({
+      caption: req.body.caption,
+      imgUrl: file.url,
+      user: req.user.id
+    })
+
+    res.status(201).json({
+      message: 'Post Created Successfully',
+      post
+    })
+  } catch (error) {
+    res.status(500).json({ message: 'Upload failed', error: error.message });
+  }
 }
--- backend/src/routes/post.route.js
+++ backend/src/routes/post.route.js
@@ -1,10 +1,11 @@
 import Router from 'express'
 import { createPostController } from '../controllers/post.controller.js';
 import uploadMiddleware from '../middlewares/upload.middleware.js';
+import authMiddleware from '../middlewares/auth.middleware.js';
 
 const postRouter = Router()
 
 // ? ==================== POST Api [protected] ======================== //
-postRouter.post('/', uploadMiddleware, createPostController)
+postRouter.post('/', authMiddleware, uploadMiddleware, createPostController)
 
 export default postRouter;
